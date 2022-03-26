e727a4e1b95cd6661616bb3e2db9a4f5f5e3819b
Lucia Seba <luciaseba0@gmail.com>
2022-03-26 20:35:13 -0300

    Firebase

--- src/components/CartCheckout/CartCheckout.js
+++ src/components/CartCheckout/CartCheckout.js
@@ -4,6 +4,8 @@ import { db } from "../../firebase/firebase";
 import { useState } from 'react';
 import { CartContext } from "../../context/cartContext";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 
 
@@ -11,19 +13,19 @@ const validate = values => {
     const errors = {};
   
     if (!values.name) {
-      errors.name = 'Required';
-    } else if (values.name.length > 15) {
-      errors.name = 'Must be 15 characters or less';
+      errors.name = 'Requerido';
+    } else if (values.name.length > 25) {
+      errors.name = 'El nombre no debe superar los 25 caracteres';
     }
   
     if (!values.phone) {
-      errors.phone = 'Required';
+      errors.phone = 'Requerido';
     } 
   
     if (!values.email) {
-      errors.email = 'Required';
+      errors.email = 'Requerido';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
+      errors.email = 'Formato de Email Inválido';
     }
   
     return errors;
@@ -34,6 +36,7 @@ export const CartCheckout =  () => {
     const [docRef, setDocRef] = useState('');
     const [error, setError] = useState('');
     const { productsIncorporated, totalPrice, clear } = useContext(CartContext);
+    const [showForm, setShowForm] = useState(true);
 
     const formik = useFormik({
         initialValues: {
@@ -44,83 +47,99 @@ export const CartCheckout =  () => {
         validate,
         onSubmit: async (values) => {
         
-        const newOrder = {
-            buyer:{
-            name:values.name,
-            phone:values.phone,
-            email:values.email,
-            },
-            items: productsIncorporated.map((item) => ({
-                id: item.id,
-                title: item.name,
-                price: item.price,
-                quantity: item.quantity,
-              })),
-              date: serverTimestamp(),
-              total: totalPrice,
-        }
-        try{
-            const data = await addDoc(collection(db, "cartOrders"), newOrder);
-            setDocRef(data);
-        }catch{
-            setError(error);
-            console.log(error);
-        }finally{
-            return(
-                    <div>
-                    <p>Compra realizada!</p> 
-                    <p>ID de seguimiento: {docRef.id}</p>
-                    </div>
+            const newOrder = {
+                buyer:{
+                name:values.name,
+                phone:values.phone,
+                email:values.email,
+                },
+                items: productsIncorporated.map((item) => ({
+                    id: item.id,
+                    title: item.name,
+                    price: item.price,
+                    quantity: item.quantity,
+                })),
+                date: serverTimestamp(),
+                total: totalPrice,
+            }
+            try{
+                const data = await addDoc(collection(db, "cartOrders"), newOrder);
+                setDocRef(data);
+            }catch{
+                setError(error);
+                console.log(error);
+            }finally{
+                setShowForm(false);
+                clear();
+            }
             
-            );
-            clear();
-        }
-        
         },
-      });
+    });
       return (
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="name">First Name</label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.name}
-          />
-          {formik.touched.name && formik.errors.name ? (
-            <div>{formik.errors.name}</div>
-          ) : null}
-    
-          <label htmlFor="phone">Last Name</label>
-          <input
-            id="phone"
-            name="phone"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.phone}
-          />
-          {formik.touched.phone && formik.errors.phone ? (
-            <div>{formik.errors.phone}</div>
-          ) : null}
-    
-          <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
-    
-          <button type="submit">Submit</button>
-        </form>
+
+        <div>
+
+            {showForm ? (
+
+            <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="name">First Name</label>
+            <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.name}
+            />
+            {formik.touched.name && formik.errors.name ? (
+                <div>{formik.errors.name}</div>
+            ) : null}
+        
+            <label htmlFor="phone">Last Name</label>
+            <input
+                id="phone"
+                name="phone"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.phone}
+            />
+            {formik.touched.phone && formik.errors.phone ? (
+                <div>{formik.errors.phone}</div>
+            ) : null}
+        
+            <label htmlFor="email">Email Address</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
+                <div>{formik.errors.email}</div>
+            ) : null}
+        
+            <button type="submit">Submit</button>
+            </form>
+       
+
+            ):(
+                <div>
+                <p>Compra realizada!</p> 
+                <p>ID de seguimiento: {docRef.id}</p>
+
+                <Link to="/tienda">
+                        <Button variant="secondary">Volver a la Tienda</Button>
+                </Link>
+               </div>
+               
+            )
+        
+        }
+        </div>
+        
       );
     
    
