fa7fcb7399fc9b0c5e9aad9b4f027f00518682ba
Lucia Seba <luciaseba0@gmail.com>
2022-03-28 00:08:39 -0300

    CSS fixes

--- src/components/CartCheckout/CartCheckout.js
+++ src/components/CartCheckout/CartCheckout.js
@@ -1,147 +1,176 @@
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
-import { useState } from 'react';
+import { useState } from "react";
 import { CartContext } from "../../context/cartContext";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, FormControl } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
+const validate = (values) => {
+  const errors = {};
 
+  if (!values.name) {
+    errors.name = "Requerido";
+  } else if (values.name.length > 25) {
+    errors.name = "El nombre no debe superar los 25 caracteres";
+  }
 
-const validate = values => {
-    const errors = {};
-  
-    if (!values.name) {
-      errors.name = 'Requerido';
-    } else if (values.name.length > 25) {
-      errors.name = 'El nombre no debe superar los 25 caracteres';
-    }
-  
-    if (!values.phone) {
-      errors.phone = 'Requerido';
-    } 
-  
-    if (!values.email) {
-      errors.email = 'Requerido';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Formato de Email Inválido';
-    }
-  
-    return errors;
-  };
+  if (!values.phone) {
+    errors.phone = "Requerido";
+  }
 
-export const CartCheckout =  () => {
+  if (!values.email) {
+    errors.email = "Requerido";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Formato de Email Inválido";
+  }
 
-    const [docRef, setDocRef] = useState('');
-    const [error, setError] = useState('');
-    const { productsIncorporated, totalPrice, clear } = useContext(CartContext);
-    const [showForm, setShowForm] = useState(true);
-
-    const formik = useFormik({
-        initialValues: {
-          name: '',
-          phone: '',
-          email: '',
-        },
-        validate,
-        onSubmit: async (values) => {
-        
-            const newOrder = {
-                buyer:{
-                name:values.name,
-                phone:values.phone,
-                email:values.email,
-                },
-                items: productsIncorporated.map((item) => ({
-                    id: item.id,
-                    title: item.name,
-                    price: item.price,
-                    quantity: item.quantity,
-                })),
-                date: serverTimestamp(),
-                total: totalPrice,
-            }
-            try{
-                const data = await addDoc(collection(db, "cartOrders"), newOrder);
-                setDocRef(data);
-            }catch{
-                setError(error);
-                console.log(error);
-            }finally{
-                setShowForm(false);
-                clear();
-            }
-            
-        },
-    });
-      return (
-
-        <div>
-
-            {showForm ? (
-
-            <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="name">First Name</label>
-            <input
-                id="name"
-                name="name"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.name}
-            />
-            {formik.touched.name && formik.errors.name ? (
-                <div>{formik.errors.name}</div>
-            ) : null}
-        
-            <label htmlFor="phone">Last Name</label>
-            <input
-                id="phone"
-                name="phone"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.phone}
-            />
-            {formik.touched.phone && formik.errors.phone ? (
-                <div>{formik.errors.phone}</div>
-            ) : null}
-        
-            <label htmlFor="email">Email Address</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-            />
-            {formik.touched.email && formik.errors.email ? (
-                <div>{formik.errors.email}</div>
-            ) : null}
-        
-            <button type="submit">Submit</button>
-            </form>
-       
-
-            ):(
-                <div>
-                <p>Compra realizada!</p> 
-                <p>ID de seguimiento: {docRef.id}</p>
-
-                <Link to="/tienda">
-                        <Button variant="secondary">Volver a la Tienda</Button>
-                </Link>
-               </div>
-               
-            )
-        
-        }
-        </div>
-        
-      );
-    
-   
+  return errors;
 };
-  
+
+export const CartCheckout = () => {
+  const [docRef, setDocRef] = useState("");
+  const [error, setError] = useState("");
+  const { productsIncorporated, totalPrice, clear } = useContext(CartContext);
+  const [showForm, setShowForm] = useState(true);
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      phone: "",
+      email: "",
+    },
+    validate,
+    onSubmit: async (values) => {
+      const newOrder = {
+        buyer: {
+          name: values.name,
+          phone: values.phone,
+          email: values.email,
+        },
+        items: productsIncorporated.map((item) => ({
+          id: item.id,
+          title: item.name,
+          price: item.price,
+          quantity: item.quantity,
+        })),
+        date: serverTimestamp(),
+        total: totalPrice,
+      };
+      try {
+        const data = await addDoc(collection(db, "cartOrders"), newOrder);
+        setDocRef(data);
+      } catch {
+        setError(error);
+        console.log(error);
+      } finally {
+        setShowForm(false);
+        clear();
+      }
+    },
+  });
+  return (
+    <div>
+      {showForm ? (
+        
+
+        <div class="row d-flex justify-content-center">
+          <h2 className="m-2">Ingresa tus datos para finalizar con tu pedido!</h2>
+          <div class="col-4">
+        <Form
+          onSubmit={formik.handleSubmit}
+          className="border rounded bg-light m-3"
+        >
+          <div class="row ">
+            <Form.Group className="m-3">
+              <div class="col-10">
+
+                <Form.Label>Nombre y Apellido</Form.Label>
+              
+                <Form.Control
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Ingresa tu nombre y apellido"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                />
+                </div>
+                {formik.touched.name && formik.errors.name ? (
+                  <div className="text-danger m-2">{formik.errors.name}</div>
+                ) : null}
+            
+              
+            </Form.Group>
+          </div>
+
+          <div class="row">
+            <Form.Group className="m-3">
+              <div class="col-10">
+                <Form.Label>Celular</Form.Label>
+             
+                <Form.Control
+                  id="phone"
+                  name="phone"
+                  type="tel"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.phone}
+                  placeholder="11-3333-4444"
+                  pattern="[0-9]{2}-[0-9]{4}-[0-9]{4}"
+                />
+              </div>
+              {formik.touched.phone && formik.errors.phone ? (
+                <div className="text-danger m-2">{formik.errors.phone}</div>
+              ) : null}
+            </Form.Group>
+          </div>
+
+          <div class="row">
+            <Form.Group className="m-3">
+          
+              <div class="col-10">
+                <Form.Label>Email</Form.Label>
+              </div>
+
+              <div class="col-10 m-0">
+                <Form.Control
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  placeholder="ejemplo@gmail.com"
+                />
+              </div>
+     
+              {formik.touched.email && formik.errors.email ? (
+                <div className="text-danger m-2">{formik.errors.email}</div>
+              ) : null}
+            </Form.Group>
+          </div>
+
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form>
+        </div>
+        </div>
+
+      ) : (
+        <div>
+          <p>Compra realizada!</p>
+          <p>ID de seguimiento: {docRef.id}</p>
+
+          <Link to="/tienda">
+            <Button variant="secondary">Volver a la Tienda</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -6,23 +6,31 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    
-    <Navbar bg="light" variant="light">
+    <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/tienda">Kawaii Geeks
-        <img src={logo} alt='logo' className="logoKawaiiGeeks" />
+        <Navbar.Brand href="/tienda">
+          Kawaii Geeks
+          <img src={logo} alt="logo" className="logoKawaiiGeeks" />
         </Navbar.Brand>
-        <Nav className="me-auto">    
-            <Nav.Link exact href="/">Inicio</Nav.Link>
-            <Nav.Link as={Link} to="/tienda">Tienda</Nav.Link>
-            <Nav.Link href="/faq">Preguntas Frecuentes</Nav.Link>
-            <Nav.Link href="/contacto">Contacto</Nav.Link>
-            <Nav.Link href="/cart"><CartWidget /></Nav.Link>
-        </Nav>
+        <div className="d-flex justify-content-end">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link exact href="/">
+                Inicio
+              </Nav.Link>
+              <Nav.Link as={Link} to="/tienda">
+                Tienda
+              </Nav.Link>
+              <Nav.Link href="/faq">Preguntas Frecuentes</Nav.Link>
+              <Nav.Link href="/contacto">Contacto</Nav.Link>
+              <Nav.Link href="/cart">
+                <CartWidget />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
-
-    
-  
   );
 };
